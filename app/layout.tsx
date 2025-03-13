3e58d9de400c34b07f836ec724179dce79acc331
vlad <vnelipovich63@gmail.com>
2025-03-13 10:35:59 +0300

    style: add dvh to main block

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${spaceMono.variable} antialiased`}>
         <Header />
-        <main className="mb-5 mt-[3rem] p-4">
+        <main className="mb-5 min-h-dvh mt-[3rem] p-4">
           <ThemeInitializer />
           {children}
         </main>
